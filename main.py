e1204a0a3b0b5e2362b279903a388ca972ba9108
Quyen Tsai <paultsai1999@gmail.com>
2021-02-15 22:15:56 -0800

    Initialized Library

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+lib = {
+    'OPERATOR': ['+', '-', '*', '/', '=', '>', '<', '%'],
+    'KEYWORDS': ['int', 'float', 'bool', 'True', 'False', 'if', 'else', 'then', 'endif', 'endelse',
+                 'while', 'whileend','do', 'enddo', 'for', 'endfor', 'STDinput', 'STDoutput', 'and', 'or', 'not'],
+    'SEPARATORS':['(', ')', '{', '}', '[', ']', ',', '.', ':', ';']
+}
