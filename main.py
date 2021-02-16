4f4e6404530a87f14cd309b19d2b0bc28395c1a6
Quyen Tsai <paultsai1999@gmail.com>
2021-02-15 22:23:13 -0800

    Initialized Library

--- main.py
+++ main.py
@@ -2,5 +2,4 @@ lib = {
     'OPERATOR': ['+', '-', '*', '/', '=', '>', '<', '%'],
     'KEYWORDS': ['int', 'float', 'bool', 'True', 'False', 'if', 'else', 'then', 'endif', 'endelse',
                  'while', 'whileend','do', 'enddo', 'for', 'endfor', 'STDinput', 'STDoutput', 'and', 'or', 'not'],
-    'SEPARATORS':['(', ')', '{', '}', '[', ']', ',', '.', ':', ';']
 }
