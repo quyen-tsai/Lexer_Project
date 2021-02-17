7cb953c547d44398edd05ce2acda6abdb1b5b20d
Aucien <blu3sag3@gmail.com>
2021-02-16 16:22:45 -0800

    Reads files and splits tokens

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
+import re
+
 lib = {
     'OPERATOR': ['+', '-', '*', '/', '=', '>', '<', '%'],
     'KEYWORDS': ['int', 'float', 'bool', 'True', 'False', 'if', 'else', 'then', 'endif', 'endelse',
-                 'while', 'whileend','do', 'enddo', 'for', 'endfor', 'STDinput', 'STDoutput', 'and', 'or', 'not'],
+                 'while', 'whileend', 'do', 'enddo', 'for', 'endfor', 'STDinput', 'STDoutput', 'and', 'or', 'not'],
     'SEPARATORS': ['(', ')', '{', '}', '[', ']', ',', '.', ':', ';']
 }
+
+
+if __name__ == "__main__":
+    with open('testFile.txt') as file:
+        line = file.readline()
+        while line:
+            test = re.findall(
+                r"[\w']+|[.,!?;]+|[\(\)\{\}\[\]]+|[+-=/<>%]", line)
+            print(test)
+            line = file.readline()
